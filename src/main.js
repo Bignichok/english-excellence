5fa7b5aca7423ee65cc7994a1a840e68b13d7afa
Nikita-Samoilenko <mykyta.samoilenko@gmail.com>
2024-10-04 23:47:28 +0300

    Mobile menu

---
+++ src/main.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuButton = document.querySelector('.mobile-menu-button');
+    const menu = document.querySelector('.menu');
+    const menuBackdrop = document.querySelector('.menu-backdrop');
+    const closeMenuButton = document.querySelector('.close-menu-button');
+    const mobileNavigation = document.querySelector('.mobile-nav-list');
+
+    const toogleMenu = () => {
+        menu.classList.toggle('active');
+        menuBackdrop.classList.toggle('active');
+    }
+
+    menuButton.addEventListener('click', toogleMenu);
+    menuBackdrop.addEventListener('click', toogleMenu);
+    closeMenuButton.addEventListener('click', toogleMenu);
+    mobileNavigation.addEventListener('click', toogleMenu);
+})
